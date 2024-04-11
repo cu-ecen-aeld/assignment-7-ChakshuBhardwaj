99b027cf961a487d135e6c862f167afd74486b41
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-04-11 00:14:51 -0400

    Updated the start stop script

--- misc-modules/module_start_stop.sh
+++ misc-modules/module_start_stop.sh
@@ -5,18 +5,22 @@ start)
     echo "Starting faulty module"
     #start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     #start-stop-daemon --start --background --exec /usr/bin/aesdsocket
-    ./lib/modules/5.15.124-yocto-standard/extra/module_load.sh faulty
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_load faulty
+    #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh faulty
 
     echo "Starting hello module"
-    ./lib/modules/5.15.124-yocto-standard/extra/module_load.sh hello
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_load hello
+    #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh hello
     ;;
     
 stop)
     echo "Stopping faulty module"
     #start-stop-daemon -K -n aesdsocket
     #start-stop-daemon --stop --exec /usr/bin/aesdsocket
-    ./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh faulty
-    ./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh hello
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_unload faulty
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_unload hello
+    #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh faulty
+    #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh hello
         ;;
 *)
     echo "Usage: $0 {start|stop}"
