3222f526f07b473cfa170c71c576dd787057283d
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-04-11 00:19:22 -0400

    Edited sh file

--- misc-modules/module_start_stop.sh
+++ misc-modules/module_start_stop.sh
@@ -5,11 +5,11 @@ start)
     echo "Starting faulty module"
     #start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     #start-stop-daemon --start --background --exec /usr/bin/aesdsocket
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_load faulty
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_load faulty
     #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh faulty
 
     echo "Starting hello module"
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_load hello
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_load hello
     #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh hello
     ;;
     
@@ -17,8 +17,8 @@ stop)
     echo "Stopping faulty module"
     #start-stop-daemon -K -n aesdsocket
     #start-stop-daemon --stop --exec /usr/bin/aesdsocket
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_unload faulty
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && module_unload hello
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_unload faulty
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_unload hello
     #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh faulty
     #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh hello
         ;;
