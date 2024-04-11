da434f2d54fb235885a6d4f82814d600b64bbe1d
Chakshu Bhardwaj <chakshu.bhardwaj93@gmail.com>
2024-04-11 00:31:07 -0400

    Added scull start stop script

--- scull/scull_start_stop.sh
+++ scull/scull_start_stop.sh
@@ -2,24 +2,24 @@
 
 case "$1" in
 start)
-    echo "Starting faulty module"
+    echo "Starting scull module"
     #start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     #start-stop-daemon --start --background --exec /usr/bin/aesdsocket
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_load faulty
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./scull_load scull
     #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh faulty
 
-    echo "Starting hello module"
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_load hello
+    #echo "Starting hello module"
+    #cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_load hello
     #./lib/modules/5.15.124-yocto-standard/extra/module_load.sh hello
     ;;
     
 stop)
-    echo "Stopping faulty module"
+    echo "Stopping scull module"
     #start-stop-daemon -K -n aesdsocket
     #start-stop-daemon --stop --exec /usr/bin/aesdsocket
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_unload faulty
-    echo "Stopping hello module"
-    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_unload hello
+    cd /lib/modules/5.15.124-yocto-standard/extra/ && ./scull_unload scull
+    #echo "Stopping hello module"
+    #cd /lib/modules/5.15.124-yocto-standard/extra/ && ./module_unload hello
     #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh faulty
     #./lib/modules/5.15.124-yocto-standard/extra/module_unload.sh hello
         ;;
